fa7f2ad902aa89c57b2079f2ad4e8869fb09eb32
astroeng <dschacht@gmail.com>
2015-06-05 07:33:04 -0400

    Updated this to work on Mac OS X. Apparently the cu.* devices have a purpose... I always assumed they were some weird Apple thing. Switching to use the "cu" device seems to have cleared up the problem with opening the serial port.

--- acquisition_control_unit/debug/host_debug.cpp
+++ acquisition_control_unit/debug/host_debug.cpp
@@ -33,9 +33,9 @@ SerialPortClass::SerialPortClass(int serial_buffer_size)
 
 int SerialPortClass::openPort(const char* device)
 {
-  serial_port = open(device, O_RDWR);// | O_NOCTTY | O_NDELAY);
-
-/*  if (serial_port != -1)
+  serial_port = open(device, O_RDWR| O_NOCTTY);// | O_NOCTTY | O_NDELAY);
+/*
+  if (serial_port != -1)
   {
     fcntl(serial_port, F_SETFL, 0);
 
@@ -54,8 +54,8 @@ int SerialPortClass::openPort(const char* device)
 
     tcsetattr(serial_port, TCSANOW, &port_settings);
   }
-*/
-  
+
+  */
   return 0;
 }
 
@@ -68,7 +68,7 @@ int SerialPortClass::readPort(unsigned char* string)
 
 int SerialPortClass::writePort(const char* string, int length)
 {
-  write(serial_port, string, length);
+  return write(serial_port, string, length);
 }
 
 int SerialPortClass::closePort()
@@ -78,9 +78,9 @@ int SerialPortClass::closePort()
   return 0;
 }
 
-typedef unsigned char u_int_8;
+typedef unsigned char  u_int_8;
 typedef unsigned short u_int_16;
-typedef unsigned int u_int_32;
+typedef unsigned int   u_int_32;
 
 typedef struct {
 short message_kind;
@@ -132,8 +132,8 @@ typedef enum
   pin_D11 = 11,
   pin_D12 = 12,
   pin_D13 = 13,
-  pin_D14 = 14,
-  pin_D15 = 15,
+  pin_Dxx = 97,
+  pin_Dyy = 98,
   pin_A0  = 14,
   pin_A1  = 15,
   pin_A2  = 16,
@@ -148,10 +148,10 @@ typedef enum
 
 typedef struct
 {
-  u_int_16 message_kind;   /* message type as defined above. */
-  u_int_8         message_count;  /* sequence count for this message type. */
-  u_int_8         data_pad_aa;    /* needed to play nice with architectures larger than 8 bit */
-  u_int_32        message_time;   /* milliSeconds since the last message. */
+  u_int_16  message_kind;   /* message type as defined above. */
+  u_int_8   message_count;  /* sequence count for this message type. */
+  u_int_8   data_pad_aa;    /* needed to play nice with architectures larger than 8 bit */
+  u_int_32  message_time;   /* milliSeconds since the last message. */
 
 } MessageHeaderType; /* This should be 8 bytes */
 
@@ -195,8 +195,8 @@ int main()
                                    0,1,2,3,4,5,6,7,8,9,10,11,12,13,0,0,
                                    analog_in,analog_in,analog_in,analog_in,
                                    analog_in,analog_in,analog_in,analog_in,
-                                   digital_in,digital_in,digital_in,digital_in,
-                                   digital_in,digital_in,digital_in,digital_in,
+                                   digital_in,digital_in,digital_in,digital_out,
+                                   digital_in,digital_in,digital_in,digital_out,
                                    digital_in,digital_in,digital_in,digital_in,
                                    digital_in,digital_in,digital_in,digital_in,
                                    1000,125,4,0};
@@ -210,11 +210,11 @@ int main()
   cout << "Declare Port" << endl;
   
   SerialPortClass serialPort(BUFFER);
-  
 
   cout << "Open Port" << endl;
   
-  serialPort.openPort("/dev/ttyACM0");
+  serialPort.openPort("/dev/cu.usbmodem1a21");
+  //serialPort.openPort("/dev/ttyACM0");
   
   sleep(4);
 
@@ -228,12 +228,12 @@ int main()
 
   stateMessage.message_kind = start_message;
   stateMessage.count = 0;
-  stateMessage.pad = 0;
-  stateMessage.time = 123;
+  stateMessage.pad   = 0;
+  stateMessage.time  = 123;
 
   serialPort.writePort((char*)&stateMessage,8);
 
-  for (int i = 0; i < 6; i++)
+  for (int i = 0; i < 60; i++)
   {
     int looper = 0;
     cout << "Read Message" << endl;
